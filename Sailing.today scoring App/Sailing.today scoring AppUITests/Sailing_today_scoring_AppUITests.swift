7dac4d209fe1e9183c2322e52d034d9377e15459
VvanDuijvendijk <valentijn@vanduijvendijk.net>
2016-09-19 00:09:46 +0200

    Upload Scoring App
    Upload empty Scoring App

---
+++ Sailing.today scoring App/Sailing.today scoring AppUITests/Sailing_today_scoring_AppUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Sailing_today_scoring_AppUITests.swift
+//  Sailing.today scoring AppUITests
+//
+//  Created by Valentijn van Duijvendijk on 19-09-16.
+//  Copyright © 2016 Valentijn van Duijvendijk. All rights reserved.
+//
+
+import XCTest
+
+class Sailing_today_scoring_AppUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
